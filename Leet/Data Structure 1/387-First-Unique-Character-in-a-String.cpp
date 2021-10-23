d7cd3b6aa731ddcc637830a6e1f35035ae6b6041
Ivan Penev <ipvdevs@gmail.com>
2021-10-23 17:06:59 +0300

    Fix typo

--- Leet/Data Structure 1/387-First-Unique-Character-in-a-String.cpp
+++ Leet/Data Structure 1/387-First-Unique-Character-in-a-String.cpp
@@ -1,51 +1,70 @@
 /**
  *  Author: Ivan Penev
- *  Source: https://leetcode.com/problems/ransom-note/
- *  Time: O(n + m)
- *  Space: O(1)
- * 
- *  Given two stings ransomNote and magazine,
- *  return true if ransomNote can be constructed
- *  from magazine and false otherwise.
- * 
- *  Each letter in magazine can only be used once
- *  in ransomNote.
+ *  Source: https://leetcode.com/problems/first-unique-character-in-a-string/
+ *  Time: O(n) - n ~ strlen
+ *  Space: O(1) - we store an array of only 26 letters for counting
+ *  
+ *  Given a string s, find the first non-repeating character
+ *  in it and return its index. If it does not exist, return -1.
  * 
  *  Examples:
  *
- *  Input: ransomNote = "a", magazine = "b"
- *  Output: false
+ *  Input: s = "leetcode"
+ *  Output: 0
  * 
- *  Input: ransomNote = "aa", magazine = "ab"
- *  Output: false
+ *  Input: s = "loveleetcode"
+ *  Output: 2
  * 
- *  Input: ransomNote = "aa", magazine = "aab"
- *  Output: true
+ *  Input: s = "aabb"
+ *  Output: -1
  * 
  *  Constraints:
- *  1 <= ransomNote.length, magazine.length <= 105
- *  ransomNote and magazine consist of lowercase
- *  English letters.
- * 
+ *   1 <= s.length <= 105
+ *   s consists of only lowercase English letters.
+ *
 */
 
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
-bool canConstruct(string ransomNote, string magazine)
+int firstUniqCharMap(string s)
 {
-    int letters[26] = {};
-    for (char ch : magazine)
-        ++letters[ch - 'a'];
+    unordered_map<char, int> uMap;
 
-    for (char ch : ransomNote)
+    for (int i = 0; i < s.length(); ++i)
+        ++uMap[s.at(i)];
+
+    for (int i = 0; i < s.length(); ++i)
     {
-        if (letters[ch - 'a'] > 0)
-            --letters[ch - 'a'];
-        else
-            return false;
+        int letter = s.at(i);
+        if (uMap[letter] == 1)
+            return i;
     }
 
-    return true;
+    return -1;
+}
+
+// Better approach
+int firstUniqCharArray(string s)
+{
+    const size_t LETTERS_COUNT = 26;
+    int letters[LETTERS_COUNT] = {};
+
+    for (char ch : s)
+        ++letters[ch - 'a'];
+
+    for (int i = 0; i < s.length(); ++i)
+        if (letters[s[i] - 'a'] == 1)
+            return i;
+
+    return -1;
+}
+
+int main()
+{
+    firstUniqCharArray("leetcode");
+
+    return 0;
 }
